b096fe503ca14d2020d980c2ce840a38a930bd81
Scott Arciszewski <scott@arciszewski.me>
2014-07-07 14:48:24 -0400

    Update sign_up.php
    Add application logic to enforce unique username constraint in case the `user_accounts` DDL isn't set up properly.

--- examples/simple_login/sign_up.php
+++ examples/simple_login/sign_up.php
@@ -9,17 +9,25 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 	// Other validation logic can take place here
 	
 	$hashed = PasswordHash::create_hash($_POST['password']);
-	$res = $DB->pQuery(
-			"INSERT INTO user_accounts (username, password) VALUES (?, ?);",
-			array($_POST['username'], $hashed)
+	$check = $DB->pQuery(
+			"SELECT * FROM user_accounts WHERE username = ?",
+			array($_POST['username'])
 		);
-	
-	if($res) {
-		$_SESSION['msg'] = "Your registration was successful!";
-		header("Location: /login.php");
-	} else {
-		$_SESSION['msg'] = "An error has occurred.";
+	if (!empty($check)) {
+		$_SESSION['msg'] = "Username already in use.";
 		header("Location: /signup.php");
+	} else {
+		$res = $DB->pQuery(
+				"INSERT INTO user_accounts (username, password) VALUES (?, ?);",
+				array($_POST['username'], $hashed)
+			);
+		if($res) {
+			$_SESSION['msg'] = "Your registration was successful!";
+			header("Location: /login.php");
+		} else {
+			$_SESSION['msg'] = "An error has occurred.";
+			header("Location: /signup.php");
+		}
 	}
 	exit;
 } else {
