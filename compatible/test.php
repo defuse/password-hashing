52a1c5c75fd41a225b3a2b9238581529922aa3a6
Stephen Chavez <stephen.chavez12@gmail.com>
2014-08-11 12:55:20 -0600

    Added compatible ruby implementation from a old pull request.

--- compatible/test.php
+++ compatible/test.php
@@ -26,8 +26,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
     require_once('PasswordHash.php');
+
     $hash = create_hash("foobar");
     $result = validate_password("foobar", $hash);
     if ($result)
