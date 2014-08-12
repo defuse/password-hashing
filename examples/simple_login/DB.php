7a77744fdc647d8c7a230bee9ced004132c50187
Taylor Hornby <havoc@defuse.ca>
2014-08-12 16:40:22 -0600

    Tabs to spaces

--- examples/simple_login/DB.php
+++ examples/simple_login/DB.php
@@ -5,21 +5,21 @@
  */
 class DB extends PDO
 {
-	/**
-	 * Prepared statement
-	 * 
-	 * @param string $statement
-	 * @param array $params
-	 * @return mixed -- array if SELECT, 
-	 */
-	public function pQuery($statement, $params = array(), $fetch_style = PDO::FETCH_ASSOC)
-	{
-		$stmt = $this->prepare($statement);
-		$exec = $stmt->execute($params);
-		if ($exec) {
-			return $stmt->fetchAll($fetch_style);
-		} else {
-			return FALSE;
-		}
-	}
+    /**
+     * Prepared statement
+     * 
+     * @param string $statement
+     * @param array $params
+     * @return mixed -- array if SELECT, 
+     */
+    public function pQuery($statement, $params = array(), $fetch_style = PDO::FETCH_ASSOC)
+    {
+        $stmt = $this->prepare($statement);
+        $exec = $stmt->execute($params);
+        if ($exec) {
+            return $stmt->fetchAll($fetch_style);
+        } else {
+            return FALSE;
+        }
+    }
 }
