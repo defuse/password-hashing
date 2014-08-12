b7cb415a20891b8025855887ca18dc72adb2ba1f
Taylor Hornby <havoc@defuse.ca>
2014-08-11 19:42:00 -0600

    Fix the other path

--- compatible/tests/test.php
+++ compatible/tests/test.php
@@ -26,7 +26,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
  * POSSIBILITY OF SUCH DAMAGE.
  */
-require_once('../PasswordHash.php');
+require_once('PasswordHash.php');
 test();
 
 function test()
