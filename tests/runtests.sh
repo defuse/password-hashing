52440a6465c132f1ec398c9dd8600c26fd4e4b82
Taylor Hornby <havoc@defuse.ca>
2014-08-12 17:44:23 -0600

    Add travis config and fix filename

---
+++ tests/runtests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "PHP"
+echo "---------------------------------------------"
+php tests/test.php
+if [ $? -ne 0 ]; then
+    echo "FAIL."
+    exit 1
+fi
+echo "---------------------------------------------"
+echo ""
+
+echo "RUBY"
+echo "---------------------------------------------"
+echo "WARNING: No tests!"
+echo "---------------------------------------------"
+echo ""
+
+echo "C#"
+echo "---------------------------------------------"
+echo "WARNING: No tests!"
+echo "---------------------------------------------"
+echo ""
+
+echo "Java"
+echo "---------------------------------------------"
+echo "WARNING: No tests!"
+echo "---------------------------------------------"
+echo ""
+
+echo "PHP<->Ruby Compatibility"
+echo "---------------------------------------------"
+ruby tests/testRubyPhpCompatibility.rb
+if [ $? -ne 0 ]; then
+    echo "FAIL."
+    exit 1
+fi
+echo "---------------------------------------------"
+echo ""
