ad12ff4be5a4775bc3f2325d3fdd0893026b0a8f
Taylor Hornby <havoc@defuse.ca>
2014-08-16 14:12:35 -0600

    Move Java tests so that they actually run

--- tests/Test.java
+++ tests/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        basicTests();
         truncatedHashTest();
     }
 
@@ -45,6 +46,52 @@ public class Test {
 
         if (badResult == false) {
             System.out.println("Truncated hash test: pass");
+        }
+    }
+
+    /**
+     * Tests the basic functionality of the PasswordHash class
+     *
+     * @param   args        ignored
+     */
+    public static void basicTests()
+    {
+        try
+        {
+            // Print out 10 hashes
+            for(int i = 0; i < 10; i++)
+                System.out.println(PasswordHash.createHash("p\r\nassw0Rd!"));
+
+            // Test password validation
+            boolean failure = false;
+            System.out.println("Running tests...");
+            for(int i = 0; i < 10; i++)
+            {
+                String password = ""+i;
+                String hash = PasswordHash.createHash(password);
+                String secondHash = PasswordHash.createHash(password);
+                if(hash.equals(secondHash)) {
+                    System.out.println("FAILURE: TWO HASHES ARE EQUAL!");
+                    failure = true;
+                }
+                String wrongPassword = ""+(i+1);
+                if(PasswordHash.validatePassword(wrongPassword, hash)) {
+                    System.out.println("FAILURE: WRONG PASSWORD ACCEPTED!");
+                    failure = true;
+                }
+                if(!PasswordHash.validatePassword(password, hash)) {
+                    System.out.println("FAILURE: GOOD PASSWORD NOT ACCEPTED!");
+                    failure = true;
+                }
+            }
+            if(failure)
+                System.out.println("TESTS FAILED!");
+            else
+                System.out.println("TESTS PASSED!");
+        }
+        catch(Exception ex)
+        {
+            System.out.println("ERROR: " + ex);
         }
     }
 }
