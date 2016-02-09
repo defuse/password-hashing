4bb7f0bcf478ea91467c90fc471629e6925da481
Taylor Hornby <taylor@defuse.ca>
2016-02-09 12:49:52 -0700

    Clarify comment about constants.

--- PasswordStorage.cs
+++ PasswordStorage.cs
@@ -25,12 +25,12 @@ namespace PasswordSecurity
 
     class PasswordStorage
     {
-        // The following constants may be changed without breaking existing
-        // hashes.
+        // These constants may be changed without breaking existing hashes.
         public const int SALT_BYTES = 24;
         public const int HASH_BYTES = 18;
         public const int PBKDF2_ITERATIONS = 64000;
 
+        // These constants define the encoding and may not be changed.
         public const int HASH_SECTIONS = 5;
         public const int HASH_ALGORITHM_INDEX = 0;
         public const int ITERATION_INDEX = 1;
